9cf7563f58b95da3d368a4eb97e9343ea27636dd
ShuKun.Li <2562017477@qq.com>
2025-03-19 18:02:28 +0800

    op

--- src/main/java/com/lip/algorithm/LCR75/LCR134.java
+++ src/main/java/com/lip/algorithm/LCR75/LCR134.java
@@ -8,7 +8,19 @@ package com.lip.algorithm.LCR75;
  * @date: 2025/3/19 17:54
  */
 public class LCR134 {
-    public double myPow(double x, int n) {
 
+    // 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。
+
+
+    public double myPow(double x, int n) {
+        long res = 1;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                res *= x;
+            }
+            x *= x;
+            n /= 2;
+        }
+        return res;
     }
 }
