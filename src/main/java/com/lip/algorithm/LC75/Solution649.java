26b588357fcfae9c4265f339bc8885435160cc19
ShuKun.Li <2562017477@qq.com>
2025-04-07 10:48:07 +0800

    op

---
+++ src/main/java/com/lip/algorithm/LC75/Solution649.java
@@ -0,0 +1,24 @@
+package com.lip.algorithm.LC75;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+import javax.jnlp.IntegrationService;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author: Elon
+ * @title: Solution649
+ * @projectName: ManyTechIWantLearnInShort
+ * @description: TODO
+ * @date: 2025/4/3 15:01
+ */
+public class Solution649 {
+
+
+    //TODO
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> queue = new LinkedList<>();
+        return null;
+    }
+}
--- src/main/java/com/lip/algorithm/LC75/Solution933.java
+++ src/main/java/com/lip/algorithm/LC75/Solution933.java
@@ -1,5 +1,8 @@
 package com.lip.algorithm.LC75;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author: Elon
  * @title: Solution933
@@ -14,7 +17,9 @@ public class Solution933 {
 /*写一个 RecentCounter 类来计算特定时间范围内最近的请求。
     请你实现 RecentCounter 类：
     RecentCounter() 初始化计数器，请求数为 0 。
-    int ping(int t) 在时间 t 添加一个新请求，其中 t 表示以毫秒为单位的某个时间，并返回过去 3000 毫秒内发生的所有请求数（包括新请求）。确切地说，返回在 [t-3000, t] 内发生的请求数。
+    int ping(int t) 在时间 t 添加一个新请求，
+    其中 t 表示以毫秒为单位的某个时间，
+    并返回过去 3000 毫秒内发生的所有请求数（包括新请求）。确切地说，返回在 [t-3000, t] 内发生的请求数。
     保证 每次对 ping 的调用都使用比之前更大的 t 值。
     示例 1：
     输入：
@@ -34,12 +39,20 @@ public class Solution933 {
     至多调用 ping 方法 104 次*/
 class RecentCounter {
 
-    public RecentCounter() {
+    private Queue<Integer> queue;
 
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
     }
 
     public int ping(int t) {
+        queue.offer(t);
+        while(queue.peek() < t - 3000){
+            queue.poll();
+        }
 
+        return queue.size();
     }
 }
 
